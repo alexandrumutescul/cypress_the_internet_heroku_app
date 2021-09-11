54f2005f4f246cc7405742ea846d16843826e0d5
Alexandru-Raul Mutescul <mutescul.alexandru@gmail.com>
2021-09-11 20:25:32 +0300

    Removed exemples

--- cypress/integration/test-exemples/mocha-chai-exemple_spec.js
+++ cypress/integration/test-exemples/mocha-chai-exemple_spec.js
@@ -2,7 +2,7 @@ describe('My First Test', function () {
     it('Returns true', function () {
         expect(true).to.equal(true)
         })
-    it('Returns false', function () {
-        expect(true).to.equal(false)
-        })
+    // it('Returns false', function () {
+    //     expect(true).to.equal(false)
+    //     })
     })
